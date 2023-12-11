39148a9b5455641f4f9ed635ae7da9d8854cd3b8
Josip Maretić <josip.maretic2011@outlook.com>
2023-12-11 14:24:14 +0100

    simple title scraping

--- olx/olx_scrape.py
+++ olx/olx_scrape.py
@@ -1,16 +1,14 @@
 from bs4 import BeautifulSoup
 
-
 def olx_scrape():
-	print("Olx scrape")
-    # with open('olx/olx_auti.htm', 'r') as f:
-    #     contents = f.read()
+    print("Olx scrape")
+    with open('olx/olx_auti.html', 'r', encoding='utf-8') as f:
+        contents = f.read()
 
-    # soup = BeautifulSoup(contents, 'html.parser')
+    soup = BeautifulSoup(contents, 'html.parser')
 
-    # # Extract data
-    # # This depends on the structure of the webpage. Here's an example of getting all paragraph texts.
-    # paragraphs = soup.find_all('p')
-    # for p in paragraphs:
-    #     print(p.get_text())
+    list_items = soup.find_all('li')
+    for item in list_items:
+        print(item.get_text())
+        
     
